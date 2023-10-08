bf6edb139f22d98f0ee4c92cbf73a778917d4960
mig <michaelleojacob@gmail.com>
2023-10-08 12:03:53 -0600

    added comments to src/auth/handleSignin/page.tsx

--- src/app/auth/handleSignin/page.tsx
+++ src/app/auth/handleSignin/page.tsx
@@ -1,6 +1,15 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+
+/**
+ * child element for account and signin ?
+ * or
+ * redirect to the correct page ?
+ *
+ * session ? <Account /> : <Signin />;
+ * or session ? redirect('/user/account/') : redirect('/auth/signin');
+ */
 
 export default async function HandleSignin() {
   const session = await getServerSession(options);
