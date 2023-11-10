46b9dba9dc60d2f5c7cd691a5aa5741d61bda424
mig <michaelleojacob@gmail.com>
2023-11-10 11:49:21 -0700

    nextauth signin works with credentials, and github

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { signIn } from "next-auth/react";
+
 export default function Signin() {
   return (
     <div>
       <div>signin</div>
+      <button onClick={() => signIn()}>signin nextauth</button>
     </div>
   );
 }
