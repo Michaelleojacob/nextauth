41a4d9b91a1323550c5c6a421fcbdf9f8e43127a
mig <michaelleojacob@gmail.com>
2023-11-10 10:41:24 -0700

    +src/app/server/page.tsx, +src/app/client/page.tsx
    created two test Links to see the console.log messages on either one.

---
+++ src/app/client/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Client() {
+  console.log("hi from src/app/client/page.tsx");
+  return (
+    <div>
+      <div>
+        this component should display a console.log in the browser console
+      </div>
+    </div>
+  );
+}
---
+++ src/app/server/page.tsx
@@ -0,0 +1,8 @@
+export default function Server() {
+  console.log("hi from src/app/server/page.tsx");
+  return (
+    <div>
+      <div>this component will do a console.log on the server</div>
+    </div>
+  );
+}
