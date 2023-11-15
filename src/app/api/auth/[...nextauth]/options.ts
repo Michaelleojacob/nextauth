7099953bdfa2eed884fe3c3cab1262a72f9c522e
mig <michaelleojacob@gmail.com>
2023-11-15 05:04:20 -0700

    fixed issue were options was always returning a user, instead of checking against the credentials passed in to it.

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -40,16 +40,9 @@ export const authOptions = {
           credentials?.password === user.pass
         ) {
           return user;
-        }
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
--- src/app/components/clientButtons/signin.tsx
+++ src/app/components/clientButtons/signin.tsx
@@ -2,7 +2,7 @@
 import { signIn } from "next-auth/react";
 
 export default function SignInButton() {
-  const handleClick = () => signIn("credentials", { name: "mig", pass: "mig" });
+  const handleClick = () => signIn("credentials", { name: "cat", pass: "cat" });
 
   return <button onClick={handleClick}>signin</button>;
 }
