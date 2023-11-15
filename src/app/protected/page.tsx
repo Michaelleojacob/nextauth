fa9c48dfe258c0d32748cb0b7b84f9e38f6d1425
mig <michaelleojacob@gmail.com>
2023-11-15 04:31:22 -0700

    middleware is protecting /protected, so I removed hardcoded redirect in /protected

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,9 +1,7 @@
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function Protected() {
   const session = await getServerSession();
-  if (!session) return redirect("/");
   return (
     <pre>
       <div>protected</div>
