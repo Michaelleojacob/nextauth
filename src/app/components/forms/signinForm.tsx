828cd67cb310ad5daa91dd638c02bec147881438
mig <michaelleojacob@gmail.com>
2023-11-15 11:19:05 -0700

    redirect on successful nextauth signin working, using useRouter.

---
+++ src/app/components/forms/signinForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function SigninForm() {
+  const [user, setUser] = useState({ name: "", pass: "" });
+  const reset = () => setUser({ name: "", pass: "" });
+  const router = useRouter();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await signIn("credentials", {
+      redirect: false,
+      name: user.name,
+      pass: user.pass,
+    });
+    if (res?.ok) router.push("/protected");
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          name
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            className="text-black"
+          />
+        </label>
+        <label>
+          password
+          <input
+            type="password"
+            name="pass"
+            onChange={handleChange}
+            className="text-black"
+          />
+        </label>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,11 @@
 import SignInButton from "../components/clientButtons/signin";
+import SigninForm from "../components/forms/signinForm";
 
 export default function Signin() {
   return (
     <div>
       <SignInButton />
+      <SigninForm />
     </div>
   );
 }
