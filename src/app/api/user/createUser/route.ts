df1ab6a7a22f8de23e7f39cf5d44e0d561565204
mig <michaelleojacob@gmail.com>
2023-11-21 09:55:35 -0700

    prisma.user.create is working. new user being created. Now to add bcrypt

--- src/app/api/user/createUser/route.ts
+++ src/app/api/user/createUser/route.ts
@@ -1,3 +1,5 @@
+import prisma from "../../../../../prisma/db";
+
 export async function GET(req: Request, res: Response) {
   console.log(req);
   return Response.json({ msg: "create user route.ts GET" });
@@ -6,5 +8,12 @@ export async function GET(req: Request, res: Response) {
 export async function POST(req: Request, res: Response) {
   const body = await req.json();
   console.log(body);
+  const { user } = body;
+  if (user.password === user.confirmpassword) {
+    const test = await prisma.user.create({
+      data: { name: user.username, password: user.password },
+    });
+    console.log(test);
+  }
   return Response.json({ msg: "create user route.ts POST" });
 }
